e1d7f8c3a56f167a46939c30fc83115d8f2360e8
KingsLandingCS <finalfantasy.fantasy9@gmail.com>
2024-07-31 23:18:14 +0500

    learning

---
+++ src/app/warning-alert/warning-alert-component.ts
@@ -0,0 +1,12 @@
+@import { Component } from 'angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  template: `
+  <p>This is a warning. You are in danger!</p>
+  `
+})
+
+export class WarningAlertComponent {
+
+}
